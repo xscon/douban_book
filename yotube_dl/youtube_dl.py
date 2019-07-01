ed0c3116fd43ba60ace42f50b0c30894c17a19e7
xscon <48077180+xscon@users.noreply.github.com>
2019-07-01 23:25:46 +0800

    update: 1:新目标只提前8集 2:简单添加对特殊视频的下载方式

--- yotube_dl/youtube_dl.py
+++ yotube_dl/youtube_dl.py
@@ -11,14 +11,19 @@ Target_list = {"https://www.youtube.com/user/tarmax82/videos": "asmr卤蛋哥",
                "https://www.youtube.com/channel/UCZlwM3g-F6AkdIZqNBlrPww/videos": "MINI工地",
                "https://www.youtube.com/channel/UCHPF0OxcMA22CQqDd8Zh4ew/videos": "PithoTv",
                "https://www.youtube.com/user/SWITCHSCISSORS/videos": "SWITCHSCISSORS",
-               "https://www.youtube.com/channel/UCMrBRvrEMDtGyqjp0U9pxgw/videos":"asmr小胡子"}
+               "https://www.youtube.com/channel/UCMrBRvrEMDtGyqjp0U9pxgw/videos": "asmr小胡子",
+               "https://www.youtube.com/channel/UCnPgVgDVyhqk5MCgxSFmEXw/videos": "Nomad ASMR",
+               "https://www.youtube.com/user/Gimmeaflakeman/videos": "asmr日本",
+               "https://www.youtube.com/user/sekotans/videos": "日本女士",
+               "https://www.youtube.com/channel/UCJ7YhtO8_nGqk1_aJHzo6Jg/videos": "印度女士"}
+
 
 ABS_PATH = os.path.dirname(os.path.abspath(__file__))
 DRIVER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 # 查询视频信息语句
 COMMAND_PREFIX_CHECK = 'youtube-dl -F '
 # 下载1080p视频语句
-COMMAND_PREFIX_DOWNLOAD = 'youtube-dl -f 137+140 '  # 有些视频不一样 #TODO:这里设置自动读取
+COMMAND_PREFIX_DOWNLOAD = 'youtube-dl -f 137+140 '
 
 
 class YoutubeDL(object):
@@ -40,7 +45,7 @@ class YoutubeDL(object):
     def get_video_list(self):  # 抓取列表-保存到指定目录下的url_list.txt
         self.web_driver = webdriver.Chrome(executable_path=DRIVER_PATH)
         self.web_driver.get(self.target_url)
-        self.roll_page()
+        # self.roll_page()
         url_temp = self.web_driver.find_elements_by_xpath('//a[@id="video-title"]')
         for url in url_temp:
             self.url_ilst.append(url.get_attribute("href"))
@@ -52,10 +57,10 @@ class YoutubeDL(object):
             fobj.write(url + "\n")
         fobj.close()
 
-    def roll_page(self):
-        js = "var q=document.documentElement.scrollTop={}".format(10000)
-        self.web_driver.execute_script(js)
-        time.sleep(1)
+    # def roll_page(self):
+    #     js = "var q=document.documentElement.scrollTop={}".format(10000)
+    #     self.web_driver.execute_script(js)
+    #     time.sleep(1)
 
     def check_txt(self):  # 查有无txt，无则新建下载列表。有则执行check_video_list
         if os.path.exists(self.Txt_path):
@@ -72,7 +77,7 @@ class YoutubeDL(object):
             print("txt文件不存在,将全新下载")
             self.get_video_list()
             self.save_txt()
-            for url in self.url_ilst:
+            for url in self.url_ilst[:8]:
                 self.download_by_url(url)
             subprocess.Popen("move *.mp4 " + self.Path, shell=True)  # 将视频转移到指定目录
 
@@ -121,10 +126,12 @@ if __name__ == '__main__':
     while True:
         for x in Target_list.keys():
             print("=" * 50)
+            if x == "https://www.youtube.com/channel/UCJ7YhtO8_nGqk1_aJHzo6Jg/videos":
+                COMMAND_PREFIX_DOWNLOAD = 'youtube-dl -f 136+140 '     # 印度女士编码不同
             t = YoutubeDL(x)
             t.main()
             print("=" * 50)
             print("\t\t")
         print(time.strftime(format("%Y-%m-%d %H:%M")), "更新")
-        print("*" * 100)
+        print("*" * 80)
         time.sleep(3600)
