9ee3a8c8680772efae47dd586405553742775f3b
xscon <48077180+xscon@users.noreply.github.com>
2019-09-05 21:44:48 +0800

    新建项目

--- douban/douban/settings.py
+++ douban/douban/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'douban.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'douban (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- douban/douban/spiders/doubanbook_spider.py
+++ douban/douban/spiders/doubanbook_spider.py
@@ -5,7 +5,23 @@ import scrapy
 class DoubanbookSpiderSpider(scrapy.Spider):
     name = 'doubanbook_spider'
     allowed_domains = ['douban.com']
-    start_urls = ['http://douban.com/']
+    start_urls = ['https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4']
 
+    # 获得所有目录页
     def parse(self, response):
+        page_count = response.xpath('//div[@class="paginator"]/a/text()').extract()[-1]
+        # for page_num in range(1, int(page_count)):
+        for page_num in range(1, 2):
+            full_cattle_url = response.url + '?start=' + str(page_num)
+            print(full_cattle_url)
+            yield scrapy.Request(url=full_cattle_url, callback=self.parse_catlle)
+
+    # 获得目录页所有链接
+    def parse_catlle(self, response):
+        print(response.url)
+        url_list = response.xpath('//div[@class="info"]/h2/a/@href').extract()
+        for url in url_list:
+            yield scrapy.Request(url=url, callback=self.parse_page)
+
+    def parse_page(self, response):
         pass
